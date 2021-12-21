89e85c03466ebfb0a2ba5e803d37c90ddb12ad9a
Seymur-Code <tu201806001@code.edu.az>
2021-12-21 15:37:31 +0400

    Gecikmish tasklar

---
+++ Method/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            // Console.WriteLine(Sum1(15, 4))
+            #endregion
+
+            #region Task 2
+            //Console.WriteLine(Sum2(3 , 11));
+            #endregion
+
+            #region Taks 3
+            //Console.WriteLine(Sum3(8 , 6));
+            #endregion
+
+            #region Taks 4
+           /* Console.WriteLine(Sum4(4, 5, 6));*/
+            #endregion
+        }
+
+        #region Task1 method
+        /*  static string Sum1(int a, int b)
+          {
+              string result = $"{a + b}   {a * b}";
+              return result;
+          }*/
+        #endregion
+
+        #region Task2 method
+        /*   static int SUm2(int a, int b)
+           {
+               int sum = 0;
+               for (int i = a; i <= b; i++)
+               {
+                   sum += i;
+               }
+               return sum;
+           }*/
+        #endregion
+
+        #region Task3 method
+        /* static int Sum3(int a, int b)
+         {
+             int sum = a + b;
+             return sum;
+         }*/
+        #endregion
+
+        #region Task4 method
+       /* static string Sum4(int a, int b, int c)
+        {
+            string sum = $"1 {a * b * c} 2 {a - b - c} 3 {a * b - c} 4 {a + b / a * c}";
+            return sum;
+        }*/
+        #endregion
+
+    }
+}
